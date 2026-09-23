c8c9f00cce6b35dee15f9256f9197f13cf76e978
leeoceantak <akrm4839@gmail.com>
2026-09-23 20:06:55 +0900

    취미 3가지를 목록으로 작성

--- HW1/app.py
+++ HW1/app.py
@@ -8,5 +8,11 @@ def introduce_template_render():
     return render_template("introduce.html")
 
 
+@app.route("/profile")
+def profile():
+    hobbies = ["영화", "운동", "게임"]
+    return render_template("profile.html", hobbies=hobbies)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
