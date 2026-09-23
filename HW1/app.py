d13b698fc9174128023997cff148c5be294d3cff
leeoceantak <akrm4839@gmail.com>
2026-09-23 20:13:16 +0900

    greet 페이지 작성

--- HW1/app.py
+++ HW1/app.py
@@ -14,5 +14,10 @@ def profile():
     return render_template("profile.html", hobbies=hobbies)
 
 
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template("greet.html", name=name)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
